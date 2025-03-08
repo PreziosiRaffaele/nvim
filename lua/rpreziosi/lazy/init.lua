a5f1df96006d0e71e3b972496bf70abad68f60c3
Raffaele Preziosi <raffaele.preziosi@randstad.com>
2025-03-08 13:00:11 +0100

    install treesitter

--- lua/rpreziosi/lazy/init.lua
+++ lua/rpreziosi/lazy/init.lua
@@ -1,6 +1,7 @@
 return {
   -- Import plugin configurations
   require("rpreziosi.lazy.tokyonight"),
+  require("rpreziosi.lazy.treesitter"),
   require("rpreziosi.lazy.telescope"),
   require("rpreziosi.lazy.today"),
 }
---
+++ lua/rpreziosi/lazy/treesitter.lua
@@ -0,0 +1,39 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  event = { "BufReadPost", "BufNewFile" },
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      -- A list of parser names, or "all" (the installed parsers)
+      ensure_installed = { 
+        "lua", "vim", "vimdoc", "query",
+        "javascript", "typescript", "python", "bash",
+        "markdown", "markdown_inline", "json", "yaml", 
+      },
+      
+      -- Install parsers synchronously (only applied to `ensure_installed`)
+      sync_install = false,
+      
+      -- Automatically install missing parsers when entering buffer
+      auto_install = true,
+      
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      
+      indent = { enable = true },
+      
+      -- Incremental selection based on the named nodes from the grammar
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = "<C-s>",
+          node_decremental = "<M-space>",
+        },
+      },
+    })
+  end,
+}
