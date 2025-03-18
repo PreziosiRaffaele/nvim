3124557de9cb4cc719b22b99736a4725c8eeaa75
Raffaele Preziosi <raffaele.preziosi@randstad.com>
2025-03-18 18:49:52 +0100

    Fix Fugitive settings

--- lua/rpreziosi/lazy/fugitive.lua
+++ lua/rpreziosi/lazy/fugitive.lua
@@ -1,9 +1,5 @@
 return {
-  "tpope/vim-fugitive",
-  cmd = { "Git", "Gstatus", "Gblame", "Gpush", "Gpull" },
-  keys = {
-    { "<leader>gs", "<cmd>Git<cr>", desc = "Git status" },
-    { "<leader>gb", "<cmd>Git blame<cr>", desc = "Git blame" },
-    { "<leader>gd", "<cmd>Gvdiffsplit<cr>", desc = "Git diff" },
-  },
+   "tpope/vim-fugitive",
+   config = function()
+  end,
 }
