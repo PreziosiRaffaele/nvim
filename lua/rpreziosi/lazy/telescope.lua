7b0160a1a9ac69192321f0e23c9f73273b2cf5dc
Raffaele Preziosi <raffaele.preziosi@randstad.com>
2025-03-21 11:11:05 +0100

    Telescope shortcut find all files

--- lua/rpreziosi/lazy/telescope.lua
+++ lua/rpreziosi/lazy/telescope.lua
@@ -11,5 +11,6 @@ return {
     vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Live grep" })
     vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find buffers" })
     vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Help tags" })
+    vim.keymap.set("n", "<leader>faf", "<cmd>Telescope find_files hidden=true no_ignore=true<cr>", { desc = "Help tags" })
   end,
 }
