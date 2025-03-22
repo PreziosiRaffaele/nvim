cff41d7e922d79d895c852b3072c1b642beb146d
Raffaele Preziosi <raffaele.preziosi@randstad.com>
2025-03-22 12:48:36 +0100

    Include new key map sf

--- lua/rpreziosi/lazy/sf.lua
+++ lua/rpreziosi/lazy/sf.lua
@@ -8,5 +8,22 @@ return {
 
   config = function()
     require('sf').setup()  -- Important to call setup() to initialize the plugin!
+
+
+    -- Set keymaps
+    local Sf = require('sf')
+    vim.keymap.set("n", "<leader><leader>", Sf.toggle_term, { desc = "terminal toggle" })
+    vim.keymap.set("n", "<leader>ss", Sf.set_target_org, { desc = "set target_org" })
+    vim.keymap.set("n", "<leader>sd", Sf.diff_in_target_org, { desc = "diff in target_org" })
+    vim.keymap.set("n", "<leader>sD", Sf.diff_in_org, { desc = "diff in org..." })
+    vim.keymap.set("n", "<leader>sp", Sf.save_and_push, { desc = "push current file" })
+    vim.keymap.set("n", "<leader>sr", Sf.retrieve, { desc = "retrieve current file" })
+
+    -- Test related keymaps
+    vim.keymap.set("n", "<leader>tsc", Sf.toggle_sign, { desc = "toggle signs for code coverage" })
+    vim.keymap.set("n", "<leader>ta", Sf.run_all_tests_in_this_file, { desc = "run all tests in this file" })
+    vim.keymap.set("n", "<leader>tA", Sf.run_all_tests_in_this_file_with_coverage, { desc = "run all test in this file with coverage" })
+    vim.keymap.set("n", "<leader>tt", Sf.run_current_test, { desc = "test this under cursor" })
+    vim.keymap.set("n", "<leader>tT", Sf.run_current_test_with_coverage, { desc = "test this under cursor with coverage info" })
   end
 }
