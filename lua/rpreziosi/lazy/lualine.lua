58f86f30d2da6d4822fe85561ad6baca8c9e8ace
Raffaele Preziosi <raffaele.preziosi@randstad.com>
2025-03-22 01:54:18 +0100

    Include target org in status bar

--- lua/rpreziosi/lazy/lualine.lua
+++ lua/rpreziosi/lazy/lualine.lua
@@ -2,6 +2,13 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
+    local function sf_status()
+      local target_org = require('sf').get_target_org()
+      local covered_percent = require('sf').covered_percent()
+      return target_org .. "(" .. covered_percent .. ")"
+    end
+
+
     require('lualine').setup {
       options = {
         icons_enabled = true,
@@ -25,7 +32,7 @@ return {
       sections = {
         lualine_a = {'mode'},
         lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = {'filename'},
+        lualine_c = {'filename', sf_status},
         lualine_x = {'encoding', 'fileformat', 'filetype'},
         lualine_y = {'progress'},
         lualine_z = {'location'}
