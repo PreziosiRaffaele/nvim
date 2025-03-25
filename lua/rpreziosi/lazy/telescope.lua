667a6a30a973cb643c280a3cc2c8f2aa92fb57d4
Raffaele Preziosi <raffaele.preziosi@randstad.com>
2025-03-26 00:40:09 +0100

    Shortcut telescope git

--- lua/rpreziosi/lazy/telescope.lua
+++ lua/rpreziosi/lazy/telescope.lua
@@ -43,6 +43,9 @@ return {
     })
     require("telescope").load_extension("fzf")
     -- Set keymaps
+    vim.keymap.set("n", "<leader>fv", "<cmd>Telescope git_bcommits<cr>", { desc = "Git BCommits" })
+    vim.keymap.set("n", "<leader>fc", "<cmd>Telescope git_commits<cr>", { desc = "Git Commits" })
+    vim.keymap.set("n", "<leader>fs", "<cmd>Telescope git_status<cr>", { desc = "Git Status" })
     vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find files" })
     vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Live grep" })
     vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find buffers" })
