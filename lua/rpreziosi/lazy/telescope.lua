e094bfe0542bf17081239c6422d35f1a0c2d04d3
Raffaele Preziosi <raffaele.preziosi@randstad.com>
2025-03-25 23:26:20 +0100

    telescope use fzf

--- lua/rpreziosi/lazy/telescope.lua
+++ lua/rpreziosi/lazy/telescope.lua
@@ -1,16 +1,26 @@
 return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
-    "nvim-lua/plenary.nvim"
+    "nvim-lua/plenary.nvim",
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make'
+    }
   },
   config = function()
-    require("telescope").setup({})
-    
+    require("telescope").setup({
+      extensions = {
+        fzf = {
+        },
+      },
+    })
+    require("telescope").load_extension("fzf")
     -- Set keymaps
     vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find files" })
     vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Live grep" })
     vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find buffers" })
     vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Help tags" })
-    vim.keymap.set("n", "<leader>faf", "<cmd>Telescope find_files hidden=true no_ignore=true<cr>", { desc = "Find all files" })
+    vim.keymap.set("n", "<leader>faf", "<cmd>Telescope find_files hidden=true no_ignore=true<cr>",
+      { desc = "Find all files" })
   end,
 }
