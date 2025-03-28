c9041f916b1d3a72ad3096d50963679a8091e992
Raffaele Preziosi <raffaele.preziosi@randstad.com>
2025-03-28 20:31:09 +0100

    Add shortcut file history include merges

--- lua/rpreziosi/lazy/diffview.lua
+++ lua/rpreziosi/lazy/diffview.lua
@@ -5,6 +5,7 @@ return {
     })
     vim.api.nvim_set_keymap("n", "<leader>do", ":DiffviewOpen<CR>",  {desc= "Open DiffView"})
     vim.api.nvim_set_keymap("n", "<leader>dc", ":DiffviewClose<CR>", {desc= "Close DiffView"})
-    vim.api.nvim_set_keymap("n", "<leader>df", ":DiffviewFileHistory --no-merges --max-count=60 %<CR>", { desc = "File History (Current File)" })
+    vim.api.nvim_set_keymap("n", "<leader>df", ":DiffviewFileHistory --no-merges --max-count=60 %<CR>", { desc = "File History (Current File) no merges" })
+    vim.api.nvim_set_keymap("n", "<leader>dm", ":DiffviewFileHistory --max-count=60 %<CR>", { desc = "File History (Current File)" })
   end,
 }
