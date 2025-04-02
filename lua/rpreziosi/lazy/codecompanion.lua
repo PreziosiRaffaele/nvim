8092712faf00a210d819246981bfe05e97c588b4
Raffaele Preziosi <raffaele.preziosi@randstad.com>
2025-04-02 22:26:32 +0200

    Code Companion shortcut to review text

--- lua/rpreziosi/lazy/codecompanion.lua
+++ lua/rpreziosi/lazy/codecompanion.lua
@@ -1,47 +1,66 @@
 return {
-  "olimorris/codecompanion.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
-  },
-  config = function()
-    require("codecompanion").setup({
-      display = {
-        chat = {
-          render_headers = false,
-        },
-      },
-      strategies = {
-        chat = {
-          adapter = "copilot",
-          slash_commands = {
-            ["file"] = {
-              opts = {
-                provider = "telescope",
-              },
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+        require("codecompanion").setup({
+            prompt_library = {
+                ["Review the code"] = {
+                    strategy = "chat",
+                    description = "Review the code and suggest improvements",
+                    prompts = {
+                        {
+                            role = "system",
+                            content =
+                            "Review the provided code for readability, efficiency, and best practices. Identify potential bugs, security vulnerabilities, and areas for optimization. Suggest improvements for maintainability and scalability.",
+                        },
+                        {
+                            role = "user",
+                            content = "Review the following code: "
+                        }
+                    },
+                }
             },
-            ["buffer"] = {
-              opts = {
-                provider = "telescope",
-              },
+            display = {
+                chat = {
+                    render_headers = false,
+                },
             },
-          },
-        },
-        inline = {
-          adapter = "copilot",
-        },
-      },
-      adapters = {
-        copilot = function()
-          return require("codecompanion.adapters").extend("copilot", {
-            schema = {
-              model = {
-                default = "claude-3.7-sonnet",
-              },
+            strategies = {
+                chat = {
+                    adapter = "copilot",
+                    slash_commands = {
+                        ["file"] = {
+                            opts = {
+                                provider = "telescope",
+                            },
+                        },
+                        ["buffer"] = {
+                            opts = {
+                                provider = "telescope",
+                            },
+                        },
+                    },
+                },
+                inline = {
+                    adapter = "copilot",
+                },
             },
-          })
-        end,
-      },
-    })
-  end,
+            adapters = {
+                copilot = function()
+                    return require("codecompanion.adapters").extend("copilot", {
+                        schema = {
+                            model = {
+                                default = "claude-3.7-sonnet",
+                            },
+                        },
+                    })
+                end,
+            },
+        })
+        vim.keymap.set("n", "<leader>cc", ':CodeCompanionChat toggle<CR>', { desc = "CodeCompanion: Toggle chat" })
+        vim.keymap.set("v", "<leader>cl", ':CodeCompanionAction<CR>', { desc = "CodeCompanion: List actions" })
+    end,
 }
