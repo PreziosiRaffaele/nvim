33e05f2d0297c9a0e4959cbda3f008dc4a390e45
Raffaele Preziosi <raffaele.preziosi@randstad.com>
2025-04-05 17:11:36 +0200

    Include OpenAI and Claude as providers

--- lua/rpreziosi/lazy/avante.lua
+++ lua/rpreziosi/lazy/avante.lua
@@ -7,15 +7,30 @@ return {
         copilot = {
             endpoint = 'https://api.githubcopilot.com/',
             model = 'claude-3.7-sonnet',
-            proxy = nil,        -- [protocol://]host[:port] Use this proxy
+            proxy = nil,            -- [protocol://]host[:port] Use this proxy
             allow_insecure = false, -- Do not allow insecure server connections
-            timeout = 30000,    -- Timeout in milliseconds
-            temperature = 0.1,  -- kinda creative
-            max_tokens = 8192,
+            timeout = 30000,        -- Timeout in milliseconds
+            temperature = 0.1,      -- kinda creative
+            max_tokens = 10000,
+        },
+        openai = {
+            endpoint = "https://api.openai.com/v1",
+            model = "gpt-4o",  -- your desired model (or use gpt-4o, etc.)
+            timeout = 30000,   -- Timeout in milliseconds, increase this for reasoning models
+            temperature = 0,
+            max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+            --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+        },
+        claude = {
+            endpoint = "https://api.anthropic.com",
+            model = "claude-3-7-sonnet-20250219",
+            timeout = 30000, -- Timeout in milliseconds
+            temperature = 0,
+            max_tokens = 10000,
         },
     },
     keys = {
-      { '<leader>aC', '<cmd>AvanteClear<cr>', desc = 'Avante - Clear' },
+        { '<leader>aC', '<cmd>AvanteClear<cr>', desc = 'Avante - Clear' },
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     build = "make",
@@ -27,9 +42,9 @@ return {
         "MunifTanjim/nui.nvim",
         --- The below dependencies are optional,
         "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-        "hrsh7th/nvim-cmp",          -- autocompletion for avante commands and mentions
-        "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-        "zbirenbaum/copilot.lua",    -- for providers='copilot'
+        "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
+        "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
+        "zbirenbaum/copilot.lua",        -- for providers='copilot'
         {
             -- support for image pasting
             "HakonHarnes/img-clip.nvim",
