2a86e1ae8a31d257db04c3a7fc6d8cc7d35a8a78
Raffaele Preziosi <raffaele.preziosi@randstad.com>
2025-04-06 14:30:50 +0200

    Use different plugin for copilot

--- lua/rpreziosi/lazy/copilot.lua
+++ lua/rpreziosi/lazy/copilot.lua
@@ -1,26 +1,23 @@
 return {
-  "github/copilot.vim",
-  cmd = "Copilot",
-  event = "InsertEnter",
-  config = function()
-    -- Basic setup
-    vim.g.copilot_no_tab_map = true
-    vim.g.copilot_assume_mapped = true
-    vim.g.copilot_tab_fallback = ""
-    
-    -- Custom keymapping for accepting suggestions
-    vim.keymap.set("i", "<C-j>", 'copilot#Accept("<CR>")', {
-      expr = true,
-      replace_keycodes = false,
-      silent = true,
-    })
-    
-    -- Additional keymaps - using keys that won't conflict with Escape
-    vim.keymap.set("i", "<C-n>", "<Plug>(copilot-next)", { silent = true })
-    vim.keymap.set("i", "<C-p>", "<Plug>(copilot-previous)", { silent = true })
-    vim.keymap.set("i", "<C-\\>", "<Plug>(copilot-dismiss)", { silent = true })
-    
-    -- Ensure ESC works normally
-    vim.keymap.set("i", "<Esc>", "<Esc>", { noremap = true, silent = true })
-  end,
+    {
+        "zbirenbaum/copilot.lua",
+        event = "InsertEnter",
+        opts = {
+            suggestion = {
+                enabled = true,
+                auto_trigger = true,
+                keymap = {
+                    -- Mac-friendly keybindings using Ctrl instead of Alt/Option
+                    accept = "<C-j>",      -- Ctrl+Enter to accept suggestion
+                    accept_word = "<C-k>", -- Ctrl+K to accept word
+                    next = "<C-]>",        -- Ctrl+N for next suggestion
+                    prev = "<C-[>",        -- Ctrl+P for previous suggestion
+                    dismiss = "<C-\\>",     -- Ctrl+E to dismiss suggestion
+                },
+            },
+            panel = {
+                enabled = true, -- disable the Copilot panel
+            },
+        },
+    },
 }
--- lua/rpreziosi/options.lua
+++ lua/rpreziosi/options.lua
@@ -5,22 +5,22 @@ local function setup()
     -- Basic editor settings
     vim.opt.number = true
     vim.opt.relativenumber = true
-    vim.opt.cursorline = true  -- Highlight current line
-    vim.opt.wrap = false       -- Don't wrap lines
-    vim.opt.expandtab = true   -- Use spaces instead of tabs
-    vim.opt.tabstop = 4        -- Number of spaces tabs count for
-    vim.opt.shiftwidth = 4     -- Size of an indent
-    vim.opt.softtabstop = 4    -- Number of spaces a tab counts for when editing
-    vim.opt.smartindent = true -- Insert indents automatically
+    vim.opt.cursorline = true    -- Highlight current line
+    vim.opt.wrap = false         -- Don't wrap lines
+    vim.opt.expandtab = true     -- Use spaces instead of tabs
+    vim.opt.tabstop = 4          -- Number of spaces tabs count for
+    vim.opt.shiftwidth = 4       -- Size of an indent
+    vim.opt.softtabstop = 4      -- Number of spaces a tab counts for when editing
+    vim.opt.smartindent = true   -- Insert indents automatically
     vim.opt.termguicolors = true -- True color support
-    vim.opt.signcolumn = "yes" -- Always show sign column
+    vim.opt.signcolumn = "yes"   -- Always show sign column
 
     -- Command-line history and completion
-    vim.opt.history = 1000     -- Store more command history
-    vim.opt.pumheight = 15     -- Maximum number of items to show in popup menu
+    vim.opt.history = 1000       -- Store more command history
+    vim.opt.pumheight = 15       -- Maximum number of items to show in popup menu
     vim.opt.inccommand = "split" -- Show incremental effects of commands
-    vim.opt.ignorecase = true  -- Ignore case when searching
-    vim.opt.smartcase = true   -- Override ignorecase when search has uppercase
+    vim.opt.ignorecase = true    -- Ignore case when searching
+    vim.opt.smartcase = true     -- Override ignorecase when search has uppercase
 
     -- Use the system clipboard by default for yank and paste
     vim.opt.clipboard = "unnamedplus"
@@ -30,6 +30,8 @@ local function setup()
     vim.keymap.set('n', '<C-p>', '<cmd>cprev<CR>', { desc = 'Previous quickfix item' })
     -- diagnostics
     vim.diagnostic.config({ virtual_text = true, signs = true })
+    -- Ensure ESC works normally no remap
+    vim.keymap.set("i", "<Esc>", "<Esc>", { noremap = true, silent = true })
 end
 
 return { setup = setup }
