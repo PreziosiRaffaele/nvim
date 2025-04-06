c779b18739c275b65e0ea7f4c1e5d8311b40498b
Raffaele Preziosi <raffaele.preziosi@randstad.com>
2025-04-06 15:04:46 +0200

    Ignore case for fzf telescope

--- lua/rpreziosi/lazy/telescope.lua
+++ lua/rpreziosi/lazy/telescope.lua
@@ -66,7 +66,7 @@ return {
           fuzzy = true,                   -- false will only do exact matching
           override_generic_sorter = true, -- override the generic sorter
           override_file_sorter = true,    -- override the file sorter
-          case_mode = "smart_case",
+          case_mode = "ignore_case",      -- or "smart_case" or "respect_case"
         },
       },
     })
