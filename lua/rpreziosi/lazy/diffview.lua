c5ad3003107004316bd914ba85546f332f2b7c2a
Raffaele Preziosi <raffaele.preziosi@randstad.com>
2025-04-08 23:52:04 +0200

    Increase max-count of commits and add stash list view shortcut

--- lua/rpreziosi/lazy/diffview.lua
+++ lua/rpreziosi/lazy/diffview.lua
@@ -10,9 +10,11 @@ return {
       { desc = "Git File History (NO MERGES)" })
     vim.api.nvim_set_keymap("n", "<leader>dm", ":DiffviewFileHistory --max-count=60 %<CR>",
       { desc = "Git File History" })
-    vim.api.nvim_set_keymap("n", "<leader>dh", ":DiffviewFileHistory --no-merges --max-count=60<CR>",
+    vim.api.nvim_set_keymap("n", "<leader>dh", ":DiffviewFileHistory --no-merges --max-count=1000<CR>",
       { desc = "Git Current Directory History (NO MERGES)" })
-    vim.api.nvim_set_keymap("n", "<leader>dl", ":DiffviewFileHistory --max-count=60<CR>",
+    vim.api.nvim_set_keymap("n", "<leader>dl", ":DiffviewFileHistory --max-count=1000<CR>",
       { desc = "Git Current Directory History" })
+    vim.api.nvim_set_keymap("n", "<leader>ds", ":DiffviewFileHistory -g --range=stash<CR>",
+      { desc = "Git Stash History" })
   end,
 }
