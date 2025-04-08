f763429420762c0c916f69857447a478817fbc76
Raffaele Preziosi <raffaele.preziosi@randstad.com>
2025-04-09 00:50:52 +0200

    Commands to show filtered commits by author

--- lua/rpreziosi/lazy/diffview.lua
+++ lua/rpreziosi/lazy/diffview.lua
@@ -16,5 +16,52 @@ return {
       { desc = "Git Current Directory History" })
     vim.api.nvim_set_keymap("n", "<leader>ds", ":DiffviewFileHistory -g --range=stash<CR>",
       { desc = "Git Stash History" })
+
+    -- Custom function to list commits by author
+    vim.api.nvim_create_user_command('DiffviewFileHistoryByAuthor', function()
+      -- Get list of authors from git
+      local authors = vim.fn.systemlist("git log --pretty='%an <%ae>' | sort -u")
+
+      -- Remove duplicates
+      local unique_authors = {}
+      local seen = {}
+      for _, author in ipairs(authors) do
+        if not seen[author] then
+          table.insert(unique_authors, author)
+          seen[author] = true
+        end
+      end
+
+      -- Let user select an author
+      vim.ui.select(unique_authors, {
+        prompt = "Select author to filter by:",
+        format_item = function(item)
+          return item
+        end
+      }, function(author)
+        if author then
+          -- Extract the email part from "Name <email>" format
+          local email = author:match("<%s*(.-)%s*>")
+          local name = author:match("^(.-)%s*<")
+
+          local filter
+          if email then
+            filter = string.format('--author="%s"', email)
+          else
+            filter = string.format('--author="%s"', author)
+          end
+
+          local cmd = string.format(":DiffviewFileHistory %s --no-merges --max-count=1000<CR>", filter)
+          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(cmd, true, false, true), "n", true)
+
+          -- Notify user about the selected author
+          vim.notify("Showing commits by " .. (name or author), vim.log.levels.INFO)
+        end
+      end)
+    end, { desc = "Show file history filtered by author" })
+
+    -- Add keymap for the new command
+    vim.api.nvim_set_keymap("n", "<leader>da", ":DiffviewFileHistoryByAuthor<CR>",
+      { desc = "Git History By Author" })
   end,
 }
