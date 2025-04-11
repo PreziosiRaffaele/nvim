ce58e3611b7f282f66ecdf8385d396e820c265a3
Raffaele Preziosi <raffaele.preziosi@randstad.com>
2025-04-11 21:07:33 +0200

    Adjust color DiffChange to make it more visible

--- lua/rpreziosi/lazy/kanagawa.lua
+++ lua/rpreziosi/lazy/kanagawa.lua
@@ -1,11 +1,15 @@
 return {
-  "rebelot/kanagawa.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require("kanagawa").setup({
-      -- You can customize the theme here
-    })
-    vim.cmd([[colorscheme kanagawa-wave]])
-  end,
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require("kanagawa").setup({
+            overrides = function(colors)
+                return {
+                    DiffChange = { bg = "#2e2f28" },       -- muted yellow-brown
+                }
+            end                                            -- You can customize the theme here
+        })
+        vim.cmd([[colorscheme kanagawa-wave]])
+    end,
 }
