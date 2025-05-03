7da68314e01d2ea86bd8704ab2de8873d6278d4e
Raffaele Preziosi <raffaele.preziosi@randstad.com>
2025-05-03 18:38:13 +0200

    Merge lazy_init in init.lua.

--- lua/rpreziosi/init.lua
+++ lua/rpreziosi/init.lua
@@ -1,4 +1,21 @@
-require("rpreziosi.lazy_init")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    spec = "rpreziosi.lazy",
+    change_detection = { notify = false }
+})
+
 -- Setup custom commands and functionality
 require("rpreziosi.gitlab").setup()
 -- Load apex_coverage module and register commands
