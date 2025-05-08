4460dc59fcf4c0349c10c7e3943b52781feb5684
Raffaele Preziosi <raffaele.preziosi@randstad.com>
2025-05-08 20:01:01 +0200

    Add key for apexCoverage.

--- lua/rpreziosi/plugins/apexCoverage.lua
+++ lua/rpreziosi/plugins/apexCoverage.lua
@@ -1,5 +1,8 @@
 return {
     cmd = { "ApexCoverage"},
+    keys = {
+        { "<leader>tc", "<cmd>ApexCoverage<cr>", desc = "Apex Coverage" },
+    },
     "PreziosiRaffaele/nvim-apex-coverage",
     dependencies = { "nvim-lua/plenary.nvim" },
 }
