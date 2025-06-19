bcc378197c37d0e4abe2098dd43a829d65c47ee8
Raffaele Preziosi <raffaele.preziosi@randstad.com>
2025-06-20 00:28:51 +0200

    Change lualine theme.

--- lua/rpreziosi/config/options.lua
+++ lua/rpreziosi/config/options.lua
@@ -1,37 +1,39 @@
 -- Set vim options
 local function setup()
     -- Set leader key to space
-    vim.g.mapleader = " "
+    vim.g.mapleader = ' '
 
     -- Basic editor settings
     vim.opt.number = true
     vim.opt.relativenumber = true
-    vim.opt.cursorline = true    -- Highlight current line
-    vim.opt.wrap = false         -- Don't wrap lines
-    vim.opt.expandtab = true     -- Use spaces instead of tabs
-    vim.opt.tabstop = 4          -- Number of spaces tabs count for
-    vim.opt.shiftwidth = 4       -- Size of an indent
-    vim.opt.softtabstop = 4      -- Number of spaces a tab counts for when editing
-    vim.opt.smartindent = true   -- Insert indents automatically
+    vim.opt.cursorline = true -- Highlight current line
+    vim.opt.wrap = false -- Don't wrap lines
+    vim.opt.expandtab = true -- Use spaces instead of tabs
+    vim.opt.tabstop = 4 -- Number of spaces tabs count for
+    vim.opt.shiftwidth = 4 -- Size of an indent
+    vim.opt.softtabstop = 4 -- Number of spaces a tab counts for when editing
+    vim.opt.smartindent = true -- Insert indents automatically
     vim.opt.termguicolors = true -- True color support
-    vim.opt.signcolumn = "yes"   -- Always show sign column
+    vim.opt.signcolumn = 'yes' -- Always show sign column
 
     -- Command-line history and completion
-    vim.opt.history = 1000       -- Store more command history
-    vim.opt.pumheight = 15       -- Maximum number of items to show in popup menu
-    vim.opt.inccommand = "split" -- Show incremental effects of commands
-    vim.opt.ignorecase = true    -- Ignore case when searching
-    vim.opt.smartcase = true     -- Override ignorecase when search has uppercase
+    vim.opt.history = 1000 -- Store more command history
+    vim.opt.pumheight = 15 -- Maximum number of items to show in popup menu
+    vim.opt.inccommand = 'split' -- Show incremental effects of commands
+    vim.opt.ignorecase = true -- Ignore case when searching
+    vim.opt.smartcase = true -- Override ignorecase when search has uppercase
+
+    -- Disable search count since is in lualine 
+    vim.opt.shortmess:append('S')
 
     -- Use the system clipboard by default for yank and paste
-    vim.opt.clipboard = "unnamedplus"
+    vim.opt.clipboard = 'unnamedplus'
 
     -- Persistent undo history
     vim.opt.undofile = true
 
     -- diagnostics
     vim.diagnostic.config({ virtual_text = true, signs = true })
-
 end
 
 return { setup = setup }
--- lua/rpreziosi/plugins/lualine.lua
+++ lua/rpreziosi/plugins/lualine.lua
@@ -5,12 +5,12 @@ return {
         local function sf_status()
             -- Cache the sf module to avoid multiple require calls
             local sf = require('sf')
-            local status = ""
+            local status = ''
 
             -- Protected call to get target organization
             local ok, target_org = pcall(sf.get_target_org)
-            if not ok or not target_org or target_org == "" then
-                return ""
+            if not ok or not target_org or target_org == '' then
+                return ''
             end
 
             -- Add organization to status
@@ -18,9 +18,9 @@ return {
 
             -- Protected call to get coverage percentage
             local ok2, covered_percent = pcall(sf.covered_percent)
-            if ok2 and covered_percent and covered_percent ~= "" then
+            if ok2 and covered_percent and covered_percent ~= '' then
                 -- Format with proper spacing and more visible brackets
-                status = status .. "(".. covered_percent .. ")"
+                status = status .. '(' .. covered_percent .. ')'
             end
 
             return status
@@ -28,20 +28,25 @@ return {
 
         local gstatus = { ahead = 0, behind = 0 }
         local function update_gstatus()
-            local Job = require 'plenary.job'
+            local Job = require('plenary.job')
             Job:new({
                 command = 'git',
                 args = { 'rev-list', '--left-right', '--count', 'HEAD...@{upstream}' },
                 on_exit = function(job, _)
                     local res = job:result()[1]
                     if type(res) ~= 'string' then
-                        gstatus = { ahead = 0, behind = 0 }; return
+                        gstatus = { ahead = 0, behind = 0 }
+                        return
                     end
-                    local ahead, behind = res:match("(%d+)%s*(%d+)")
-                    if not ahead or not behind then ahead, behind = 0, 0 end
+                    local ahead, behind = res:match('(%d+)%s*(%d+)')
+                    if not ahead or not behind then
+                        ahead, behind = 0, 0
+                    end
                     gstatus = { ahead = tonumber(ahead), behind = tonumber(behind) }
                     -- Force lualine refresh
-                    vim.schedule(function() require('lualine').refresh() end)
+                    vim.schedule(function()
+                        require('lualine').refresh()
+                    end)
                 end,
             }):start()
         end
@@ -53,12 +58,10 @@ return {
         end
         _G.Gstatus_timer:start(0, 2000, vim.schedule_wrap(update_gstatus))
 
-        require('lualine').setup {
+        require('lualine').setup({
             options = {
-                icons_enabled = true,
-                theme = 'horizon',
-                component_separators = { left = '', right = '' },
-                section_separators = { left = '', right = '' },
+                icons_enabled = false,
+                theme = 'auto',
                 disabled_filetypes = {
                     statusline = {},
                     winbar = {},
@@ -71,25 +74,33 @@ return {
                     statusline = 100,
                     tabline = 100,
                     winbar = 100,
-                }
+                },
             },
             sections = {
                 lualine_a = { 'mode' },
-                lualine_b = { 'branch', function()
-                    if gstatus.ahead > 0 then
-                        return '↑' .. tostring(gstatus.ahead)
-                    end
-                    return ''
-                end, function()
-                    if gstatus.behind > 0 then
-                        return '↓' .. tostring(gstatus.behind)
-                    end
-                    return ''
-                end },
-                lualine_c = { 'filename', 'diff', 'diagnostics' },
-                lualine_x = { sf_status, 'fileformat', 'filetype' },
-                lualine_y = { 'progress' },
-                lualine_z = { 'location' }
+                lualine_b = {
+                    'branch',
+                    function()
+                        if gstatus.ahead > 0 then
+                            return '↑' .. tostring(gstatus.ahead)
+                        end
+                        return ''
+                    end,
+                    function()
+                        if gstatus.behind > 0 then
+                            return '↓' .. tostring(gstatus.behind)
+                        end
+                        return ''
+                    end,
+                },
+                lualine_c = {
+                    'filename',
+                    'diff',
+                    'diagnostics',
+                },
+                lualine_x = { 'searchcount', sf_status},
+                lualine_y = { 'filetype' },
+                lualine_z = { 'location' },
             },
             inactive_sections = {
                 lualine_a = {},
@@ -97,12 +108,12 @@ return {
                 lualine_c = { 'filename' },
                 lualine_x = { 'location' },
                 lualine_y = {},
-                lualine_z = {}
+                lualine_z = {},
             },
             tabline = {},
             winbar = {},
             inactive_winbar = {},
-            ns = {}
-        }
+            ns = {},
+        })
     end,
 }
