334f23c2eb269666b723b885ce231c2eff5c380b
Raffaele Preziosi <raffaele.preziosi@randstad.com>
2025-06-19 17:48:04 +0200

    New theme rose-pines.

--- lua/rpreziosi/plugins/themes.lua
+++ lua/rpreziosi/plugins/themes.lua
@@ -2,8 +2,8 @@ return {
     {
         'projekt0n/github-nvim-theme',
         name = 'github-theme',
-        lazy = false,
-        priority = 1000,
+        lazy = true,
+        priority = 900,
         config = function()
             require('github-theme').setup({
                 options = {
@@ -12,25 +12,34 @@ return {
                         comments = 'italic',
                         keywords = 'bold',
                         types = 'italic',
-                    }
-                }
+                    },
+                },
             })
-            vim.cmd('colorscheme github_dark_default')
+            -- vim.cmd('colorscheme github_dark_default')
         end,
     },
     {
-        "rebelot/kanagawa.nvim",
-        lazy = true,    -- Changed from false to true to disable auto-loading
+        'rebelot/kanagawa.nvim',
+        lazy = true, -- Changed from false to true to disable auto-loading
         priority = 900, -- Lower priority than github theme
         config = function()
-            require("kanagawa").setup({
+            require('kanagawa').setup({
                 overrides = function(colors)
                     return {
-                        DiffChange = { bg = "#2e2f28" },
+                        DiffChange = { bg = '#2e2f28' },
                     }
-                end
+                end,
             })
-            -- vim.cmd([[colorscheme kanagawa-wave]]) 
+            -- vim.cmd([[colorscheme kanagawa-wave]])
         end,
-    }
+    },
+    {
+        'rose-pine/neovim',
+        name = 'rose-pine',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end,
+    },
 }
