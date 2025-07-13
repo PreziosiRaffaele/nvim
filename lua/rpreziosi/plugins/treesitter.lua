5387bb2682c7bd392c0f80252f122a1ef955f074
Raffaele Preziosi <raffaele.preziosi@randstad.com>
2025-07-13 13:26:48 +0200

    Remove treesitter context.

--- lua/rpreziosi/plugins/treesitter.lua
+++ lua/rpreziosi/plugins/treesitter.lua
@@ -2,14 +2,31 @@ return {
     'nvim-treesitter/nvim-treesitter',
     dependencies = {
         'nvim-treesitter/nvim-treesitter-textobjects',
-        'nvim-treesitter/nvim-treesitter-context'
     },
     build = ':TSUpdate',
     config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = { "apex", "regex", "gitcommit", "bash", "haskell", "nix", "rust", "soql", "sosl", "javascript", "typescript", "lua", "vim", "vimdoc", "markdown", "json", "jsonc", "html" },
+        require('nvim-treesitter.configs').setup({
+            ensure_installed = {
+                'apex',
+                'regex',
+                'gitcommit',
+                'bash',
+                'haskell',
+                'nix',
+                'rust',
+                'soql',
+                'sosl',
+                'javascript',
+                'typescript',
+                'lua',
+                'vim',
+                'vimdoc',
+                'markdown',
+                'json',
+                'jsonc',
+                'html',
+            },
             auto_install = true,
-
             highlight = {
                 enable = true,
                 additional_vim_regex_highlighting = false,
@@ -19,15 +36,12 @@ return {
                     enable = true,
                     lookahead = true,
                 },
-
                 move = {
                     enable = true,
                     set_jumps = true, -- whether to set jumps in the jumplist
-                                 },
-
+                },
                 swap = {
                     enable = true,
-
                 },
             },
         })
